ef37ca7f4a27606c460d5293d757a4525c77f207
Prabin Timsina <prabin.tims@gmail.com>
2025-04-23 20:58:01 -0700

    Provide Logger via koin

--- src/main/kotlin/api/bank/modules/PluginModule.kt
+++ src/main/kotlin/api/bank/modules/PluginModule.kt
@@ -6,10 +6,12 @@ import api.bank.utils.dispatcher.DefaultDispatcherProvider
 import api.bank.utils.dispatcher.DispatcherProvider
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.intellij.openapi.diagnostic.Logger
 import org.koin.dsl.module
 
 val pluginModule = module {
     single<DispatcherProvider> { DefaultDispatcherProvider() }
     single<CoreRepository> { DefaultCoreRepository(get()) }
     single<Gson> { GsonBuilder().setPrettyPrinting().create() }
+    single<Logger> { Logger.getInstance("api.bank") }
 }
